459f38e52ecfa9f3048958d340ceafa7c4d15926
JoaoMafra07 <jrlorimafra@gmail.com>
2026-03-02 10:47:13 -0300

    Conectando auth_user com Usuario

--- back/api/serializers.py
+++ back/api/serializers.py
@@ -1,10 +1,55 @@
 from .models import Usuario, Imovel, Contrato, Pagamento
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = '__all__'
+
+class RegisterSerializer(serializers.Serializer):
+    #campos da tabela AUTH_USER
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField(write_one=True)
+
+    #campos da tabela Usuario
+    nome = serializers.CharField(required=False, allow_blank=True, default='') # required por padrão já é False
+    telefone = serializers.CharField(required=False, allow_blank=True, default='')
+    tipo = serializers.ChoiceField(choices=Usuario.TIPO_CHOICES)
+
+    def create(self, validated_data):
+        nome = validated_data.get('nome', '')
+        telefone = validated_data.get('telefone', '')
+        tipo = validated_data['tipo'] # Está entre colchetes pq não eh um CharField
+        email = validated_data['email']
+
+        #Criando usuário na tabela auth_user
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=email,
+            password=validated_data['password']
+        )
+
+        if tipo == 'LOCADOR':
+            user.is_staff = True
+        else:
+            user.is_staff = False
+            
+        user.is_active = True
+        user.is_superuser = False
+        user.save()
+
+        # Criando usuário na tebala Usuario
+        Usuario.objects.create(
+            user=user,
+            nome=nome if nome else user.username,
+            email=email,
+            telefone=telefone,
+            tipo=tipo
+        )
+    
+        return user
 
 class ImovelSerializer(serializers.ModelSerializer):
     class Meta:
